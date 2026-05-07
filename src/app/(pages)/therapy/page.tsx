d8ba7dd3fece09056106abf84f8553b90cedeced
Dzmitry Sviryn <dzmitry@digitalnomadicsolutions.com>
2026-05-07 22:20:59 +0300

    chore: frontpage changes

--- src/app/(pages)/therapy/page.tsx
+++ src/app/(pages)/therapy/page.tsx
@@ -3,22 +3,22 @@ import { SubmitSection } from "@/app/components/sections/SubmitSection";
 import { TherapySection } from "@/app/components/sections/TherapySection";
 
 export const metadata = {
-  title: "Направления психотерапии",
-  description: "Методики, которые я использую в практике.",
+	title: "Методики",
+	description: "Методики, которые я использую в практике.",
 };
 
 export default function Therapy() {
-  return (
-    <div
-      id="page_container"
-      className="mx-auto flex min-h-screen max-w-7xl flex-col gap-y-16 px-4 pt-[4rem] md:gap-y-20 md:px-10"
-    >
-      <TherapySection
-        title="Направления психотерапии"
-        description="Методики, которые я использую в практике:"
-      />
-      <Spacer size="sm" />
-      <SubmitSection />
-    </div>
-  );
+	return (
+		<div
+			id="page_container"
+			className="mx-auto flex min-h-screen max-w-7xl flex-col gap-y-16 px-4 pt-[4rem] md:gap-y-20 md:px-10"
+		>
+			<TherapySection
+				title="Методики"
+				description="Методики, которые я использую в практике:"
+			/>
+			<Spacer size="sm" />
+			<SubmitSection />
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,29 @@
-import { HelpSection } from "@/app/components/sections/HelpSection";
-import { TherapySection } from "@/app/components/sections/TherapySection";
-import { SubmitSection } from "@/app/components/sections/SubmitSection";
-import { HeroSection } from "@/app/components/sections/HeroSection";
 import { FeaturedPostSection } from "@/app/components/sections/FeaturedPostSection";
+import { HelpSection } from "@/app/components/sections/HelpSection";
+import { HeroSection } from "@/app/components/sections/HeroSection";
+import { SubmitSection } from "@/app/components/sections/SubmitSection";
+import { TherapySection } from "@/app/components/sections/TherapySection";
 
 export default function HomePage() {
-  return (
-    <div
-      id="page_container"
-      className="mx-auto flex min-h-screen max-w-7xl flex-col gap-y-16 px-4 pt-[4rem] md:gap-y-20 md:px-10"
-    >
-      <HeroSection />
-      <HelpSection
-        title="Помощь"
-        description="Я работаю с следующими проблемами:"
-      />
-      <TherapySection
-        title="Направления психотерапии"
-        description="Методики, которые я использую в практике:"
-      />
-      <FeaturedPostSection
-        title="Избранные статьи"
-        description="Ознакомьтесь с более чем 150 статьями в моем блоге:"
-      />
-      <SubmitSection />
-    </div>
-  );
+	return (
+		<div
+			id="page_container"
+			className="mx-auto flex min-h-screen max-w-7xl flex-col gap-y-16 px-4 pt-[4rem] md:gap-y-20 md:px-10"
+		>
+			<HeroSection />
+			<HelpSection
+				title="Помощь"
+				description="Я работаю с следующими проблемами:"
+			/>
+			<TherapySection
+				title="Методики"
+				description="Методики, которые я использую в практике:"
+			/>
+			<FeaturedPostSection
+				title="Избранные статьи"
+				description="Ознакомьтесь с более чем 150 статьями в моем блоге:"
+			/>
+			<SubmitSection />
+		</div>
+	);
 }
