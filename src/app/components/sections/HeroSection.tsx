50e485ff56daba8a06f6fd63ef4e265ff79c1a1b
Dzmitry Sviryn <dzmitry@digitalnomadicsolutions.com>
2026-05-28 21:34:41 +0300

    chore: smth

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -1,44 +1,51 @@
 import Image from "next/image";
-import { H2, H4 } from "@/app/components/Typography";
 import { ButtonLink } from "@/app/components/Buttons";
+import { H2, H4 } from "@/app/components/Typography";
 
 export function HeroSection({ isFront = true }: { isFront?: boolean }) {
-  return (
-    <div className="flex flex-col-reverse space-y-10 lg:flex-row lg:justify-between lg:space-y-0">
-      <div className="mr-14 items-center space-y-2 lg:space-y-0">
-        <div className="flex-flex-col">
-          <H2>Я-врач-психотерапевт  Валерий Гринь</H2>
-            <H4 variant="secondary" className="mt-3">Веду прием как психолог (индивидуальный предприниматель).</H4>
-          <H4 variant="secondary">
-            Я помогу оставить в прошлом Ваши проблемы, тревоги и страхи!
-          </H4>
-          <p className="mt-6 font-semibold text-lg">Помощь, поддержка,  психообразование</p>
-          <ul className="text-lg list-disc text-black list-inside">
-            <li>в депрессии, состоянии подавленности и безысходности;</li>
-            <li>в стрессе и кризисе личностном и межличностных отношений;</li>
-            <li>в конфликте с родными и близкими;</li>
-          </ul>
+	return (
+		<div className="flex flex-col-reverse space-y-10 lg:flex-row lg:justify-between lg:space-y-0">
+			<div className="mr-14 items-center space-y-2 lg:space-y-0">
+				<div className="flex-flex-col">
+					<H2>
+						Я-врач-психотерапевт Валерий Гринь (медицинская специализация по
+						психотерапии, кандидат наук)
+					</H2>
+					<H4 variant="secondary" className="mt-3">
+						Веду прием как психолог (индивидуальный предприниматель).
+					</H4>
+					<H4 variant="secondary">
+						Я помогу оставить в прошлом Ваши проблемы, тревоги и страхи!
+					</H4>
+					<p className="mt-6 font-semibold text-lg">
+						Помощь, поддержка, психообразование
+					</p>
+					<ul className="text-lg list-disc text-black list-inside">
+						<li>в депрессии, состоянии подавленности и безысходности;</li>
+						<li>в стрессе и кризисе личностном и межличностных отношений;</li>
+						<li>в конфликте с родными и близкими;</li>
+					</ul>
 
-          <div className="mt-14 flex grid-cols-9 flex-col space-y-4">
-            <div className="mr-auto flex flex-col gap-4 ">
-              {isFront && (
-                <ButtonLink to="/about-me" title="Обо мне" variant="primary" />
-              )}
-              <ButtonLink to="/blog" title="Мои статьи" variant="secondary" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="shrink-0 pb-14 lg:pb-0">
-        <Image
-          src="/doc.webp"
-          height={320}
-          width={320}
-          alt="Психотерапевт Валерий Гринь"
-          priority
-          className="aspect-square h-80 w-80"
-        />
-      </div>
-    </div>
-  );
+					<div className="mt-14 flex grid-cols-9 flex-col space-y-4">
+						<div className="mr-auto flex flex-col gap-4 ">
+							{isFront && (
+								<ButtonLink to="/about-me" title="Обо мне" variant="primary" />
+							)}
+							<ButtonLink to="/blog" title="Мои статьи" variant="secondary" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="shrink-0 pb-14 lg:pb-0">
+				<Image
+					src="/doc.webp"
+					height={320}
+					width={320}
+					alt="Психотерапевт Валерий Гринь"
+					priority
+					className="aspect-square h-80 w-80"
+				/>
+			</div>
+		</div>
+	);
 }
